7424dd74c90a0a7f123aec4351756eb15341f4b1
trungtn1 <trungtn@vtvlive.vn>
2023-10-11 13:51:18 +0700

    Initial Commit

---
+++ iOS_SDKLogin/iOS_SDKLogin.h
@@ -0,0 +1,18 @@
+//
+//  iOS_SDKLogin.h
+//  iOS_SDKLogin
+//
+//  Created by Tn Trung on 11/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOS_SDKLogin.
+FOUNDATION_EXPORT double iOS_SDKLoginVersionNumber;
+
+//! Project version string for iOS_SDKLogin.
+FOUNDATION_EXPORT const unsigned char iOS_SDKLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_SDKLogin/PublicHeader.h>
+
+
